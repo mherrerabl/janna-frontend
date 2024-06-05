650db6b61a714fdc2a872de9b0e60e5c12dfe11d
Míriam Herrera Blanes <mherrerabl@uoc.edu>
2024-06-05 19:51:15 +0200

    v30.22

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: 'https://18.101.19.115',
+  API_URL: 'https://18.101.139.42',
   stripe_public:
     'pk_test_51PFKQiEj6XaLEnGsVZ2mZpIfbaKeCAlVB3WHjAe1DwCVCrDJ9GEePSuX4bzOrTXghIDvuaBB1BoKYAARFwIdaDlb0090pynHDz',
 };
